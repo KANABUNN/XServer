9173079d703c1bdfeca42336400af0660057f9a9
KANABUNN <kanabun.kenshi@gmail.com>
2026-04-22 12:53:40 +0900

    feat: SMTP設定を追加し、不要なToDo管理設定ファイルを削除

--- apps/config_upload.php
+++ apps/config_upload.php
@@ -2,26 +2,29 @@
 declare(strict_types=1);
 
 return [
-    'smtp_host'   => '',
+    'smtp_host'   => 'sv16171.xserver.jp',
     'smtp_port'   => 465,
     'smtp_secure' => 'ssl',
-    'smtp_user'   => '',
+    'smtp_user'   => 'info@fit-sc.jp',
     'smtp_pass'   => '',
 
-    'from_addr'   => '',
+    'from_addr'   => 'info@fit-sc.jp',
     'from_name'   => '貸し部屋予約システム',
     'reservation_admin_notify_to' => 'sogokanri@bene.fit.ac.jp',
 
     'db' => [
         'dsn' => 'mysql:host=localhost;dbname=fitsc_book;charset=utf8mb4',
-        'user' => '',
+        'user' => 'fitsc_book',
         'password' => '',
     ],
 
-    'forms_db' => [
-        'dsn' => 'mysql:host=localhost;dbname=fitsc_forms;charset=utf8mb4',
-        'user' => '',
-        'password' => '',
+    'account_db' => [
+       'host' => 'localhost',
+       'port' => 3306,
+       'dbname' => 'fitsc_account',
+       'charset' => 'utf8mb4',
+       'user' => 'fitsc_admin',
+       'pass' => '',
     ],
 
     'reservation' => [
@@ -30,8 +33,6 @@ return [
         'booking_min_days_before' => 2,
         'booking_max_months_ahead' => 2,
         'time_step_minutes' => 15,
-        'booking_time_start' => '09:00',
-        'booking_time_end' => '20:00',
         'access_code_digits' => 6,
         'access_code_padding_minutes' => 10,
         'switchbot_required_for_confirmation' => true,
@@ -39,14 +40,14 @@ return [
 
     'google_calendar' => [
         'enabled' => true,
-        'gas_url' => 'https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/exec',
-        'shared_secret' => 'CHANGE_ME_TO_LONG_RANDOM_SECRET',
+        'gas_url' => 'https://script.google.com/a/macros/fit-sc.jp/s/AKfycbyjUhRHiX_ROqCGDvqOVty1P2ocSsu3kaywRX8fu355CgSWgESDrr_EgEI1tvWRH4kOdA/exec',
+        'shared_secret' => '',
         'timezone' => 'Asia/Tokyo',
         'connect_timeout' => 5,
         'timeout' => 15,
         'room_calendar_map' => [
-            'tamoku' => 'tamoku_calendar_id@group.calendar.google.com',
-            'orange' => 'orange_calendar_id@group.calendar.google.com',
+        'tamoku' => '@resource.calendar.google.com',
+        'orange' => '@resource.calendar.google.com',
         ],
     ],
 
@@ -59,8 +60,8 @@ return [
         'storage_dir' => dirname(__DIR__) . '/storage/switchbot',
         'detail_dir' => dirname(__DIR__) . '/storage/switchbot/requests',
         'request_table' => 'switchbot_passcode_requests',
-        'webhook_secret' => 'CHANGE_ME_TO_LONG_RANDOM_TOKEN',
-        'webhook_url' => '',
+        'webhook_secret' => 'YHyLNp4yfLGf3MYnNRnJ3fy5pEPEAF54GPtFPhNH',
+        'webhook_url' => 'https://set.book.fit-sc.jp/switchbot_webhook.php?token=YHyLNp4yfLGf3MYnNRnJ3fy5pEPEAF54GPtFPhNH',
         'webhook_path' => '/switchbot_webhook.php',
         'keypads' => [
             'tamoku' => [
